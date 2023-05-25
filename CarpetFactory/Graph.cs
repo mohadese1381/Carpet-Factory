e2384935b184311497f6d6c7bdb4df0019888200
Zahra <zahraamasoumi@gmail.com>
2023-05-25 20:51:45 +0330

    fixed graph and m-coloring

---
+++ CarpetFactory/Graph.cs
@@ -0,0 +1,65 @@
+﻿namespace CarpetFactory;
+
+class Graph
+{
+    private int numVertices;
+    private List<int>[] adjList;
+
+    public Graph(int numVertices)
+    {
+        this.numVertices = numVertices;
+        adjList = new List<int>[numVertices];
+        for (int i = 0; i < numVertices; i++)
+        {
+            adjList[i] = new List<int>();
+        }
+    }
+
+    public void AddEdge(int u, int v)
+    {
+        adjList[u].Add(v);
+        adjList[v].Add(u);
+    }
+
+    public bool ColorGraph(int vertex, int[] colors, int numColors)
+    {
+        if (vertex == numVertices) // All vertices have been colored
+        {
+            return true;
+        }
+
+        // Try all possible colors
+        for (int i = 0; i < numColors; i++)
+        {
+            if (IsValidColor(vertex, colors, i))
+            {
+                colors[vertex] = i;
+
+                // Recursively color the remaining vertices
+                if (ColorGraph(vertex + 1, colors, numColors))
+                {
+                    return true;
+                }
+
+                // If we can't color the remaining vertices with this color, backtrack
+                colors[vertex] = -1;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsValidColor(int vertex, int[] colors, int color)
+    {
+        // Check if any adjacent vertices have the same color
+        foreach (int adjVertex in adjList[vertex])
+        {
+            if (colors[adjVertex] == color)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- CarpetFactory/Program.cs
+++ CarpetFactory/Program.cs
@@ -1,24 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CarpetFactory;
+
 //test of m-coloring-------------------------------------------------------
-UndirectedGenericGraph graph = new UndirectedGenericGraph(4);
+// UndirectedGenericGraph graph = new UndirectedGenericGraph(4);
+//
+// graph.AddVertex(new Vertex(0));
+// graph.AddVertex(new Vertex(1));
+// graph.AddVertex(new Vertex(2));
+// graph.AddVertex(new Vertex(3));
+//
+// graph.AddEdge(graph.Vertices[0], graph.Vertices[1]);
+// graph.AddEdge(graph.Vertices[0], graph.Vertices[3]);
+// graph.AddEdge(graph.Vertices[1], graph.Vertices[2]);
+// graph.AddEdge(graph.Vertices[1], graph.Vertices[3]);
+// graph.AddEdge(graph.Vertices[2], graph.Vertices[3]);
+//
+// FactoryManager.V = 4;
+// Console.WriteLine(graph.AdjMatrix.Length);
+// int[] colors = new int[graph.Size];
+// FactoryManager.m_Coloring(graph.AdjMatrix, 3, 0, colors);
 
-graph.AddVertex(new Vertex(0));
-graph.AddVertex(new Vertex(1));
-graph.AddVertex(new Vertex(2));
-graph.AddVertex(new Vertex(3));
+int numVertices = 5;
+Graph graph = new Graph(numVertices);
+graph.AddEdge(0, 1);
+graph.AddEdge(0, 2);
+graph.AddEdge(1, 2);
+graph.AddEdge(1, 3);
+graph.AddEdge(2, 3);
+graph.AddEdge(3, 4);
 
-graph.AddEdge(graph.Vertices[0], graph.Vertices[1]);
-graph.AddEdge(graph.Vertices[0], graph.Vertices[3]);
-graph.AddEdge(graph.Vertices[1], graph.Vertices[2]);
-graph.AddEdge(graph.Vertices[1], graph.Vertices[3]);
-graph.AddEdge(graph.Vertices[2], graph.Vertices[3]);
+int[] colors = new int[numVertices];
+for (int i = 0; i < numVertices; i++)
+{
+    colors[i] = -1; // Initialize all colors to -1 (unassigned)
+}
 
-FactoryManager.V = 4;
-Console.WriteLine(graph.AdjMatrix.Length);
-int[] colors = new int[graph.Size];
-FactoryManager.m_Coloring(graph.AdjMatrix, 3, 0, colors);
+int numColors = 3; // Maximum number of colors to use
+if (graph.ColorGraph(0, colors, numColors))
+{
+    Console.WriteLine("Vertex Color");
+    for (int i = 0; i < numVertices; i++)
+    {
+        Console.WriteLine(i + "  ->  " + colors[i]);
+    }
+}
+else
+{
+    Console.WriteLine("No valid coloring found");
+}
 
 //test of knapsack-------------------------------------------------------
 int[] value = { 10, 50, 70 };
@@ -27,4 +57,4 @@ int capacity = 40;
 int itemsCount = 3;
 
 int result = FactoryManager.KnapSack(capacity, weight, value, itemsCount);
-Console.WriteLine("Knapsack result : "+result);
+Console.WriteLine("Knapsack result : " + result);
